ff8a13e930ed73d7efdd61d66404e1948b06184e
aqilaazza <aqilanurazza123@gmail.com>
2026-07-03 10:46:16 +0700

    add migrate table kategori instansi to form proposal

---
+++ database/migrations/2026_07_03_031804_add_kategori_instansi_to_proposal_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('proposal', function (Blueprint $table) {
+            $table->string('kategori_instansi')
+                ->nullable()
+                ->after('judul');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('proposal', function (Blueprint $table) {
+            $table->dropColumn('kategori_instansi');
+        });
+    }
+};
