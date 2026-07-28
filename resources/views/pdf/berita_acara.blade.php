94c982adaa8823d05f49cc85ee2ee12580108d31
karinaika <karinaikaindasa@gmail.com>
2026-07-28 14:34:04 +0700

    fixed template pdf BA

--- resources/views/pdf/berita_acara.blade.php
+++ resources/views/pdf/berita_acara.blade.php
@@ -11,7 +11,7 @@
            PAGE / MARGIN
         ===================================================== */
         @page {
-            margin-top: 4cm;
+            margin-top: 4.3cm;
             margin-bottom: 2cm;
             margin-left: 2.2cm;
             margin-right: 2.2cm;
@@ -210,15 +210,19 @@
         /* =====================================================
            TANDA TANGAN
         ===================================================== */
-        .ttd {
-            width: 100%;
+       .ttd {
+            width: calc(100% + 2cm);
+            margin-left: -1cm;
             border-collapse: collapse;
-            margin-top: -5px !important;
+            margin-top: 5px !important;
+            table-layout: fixed;
         }
 
         .ttd td {
+            width: 50%;
             text-align: center;
-            padding-top: 25px;
+            vertical-align: top;
+            padding-top: 20px;
         }
 
         .spasi-ttd td {
@@ -375,19 +379,19 @@
         <ol class="csr-list-custom">
 
             <li>
-                <strong>PARA PIHAK</strong> menyepakati bahwa pada saat melaksanakan program Corporate Social
-                Responsibility ini berdasarkan pada prinsip itikad baik, tidak saling mempengaruhi baik langsung maupun
+                <strong>PARA PIHAK</strong> menyepakati bahwa pada saat melaksanakan program <i>Corporate Social
+                Responsibility</i> ini berdasarkan pada prinsip itikad baik, tidak saling mempengaruhi baik langsung maupun
                 tidak langsung guna memenuhi keinginannya, menerima serta bertanggungjawab atas segala keputusan yang
                 ditetapkan sesuai dengan kesepakatan <strong>PARA PIHAK</strong>, menghindari serta mencegah terjadinya
-                pertentangan kepentingan (conflict of interest), menghindari serta mencegah penyalahgunaan wewenang
+                pertentangan kepentingan (<i>conflict of interest</i>), menghindari serta mencegah penyalahgunaan wewenang
                 dan/atau kolusi dan/atau korupsi dengan tujuan untuk keuntungan pribadi-golongan-atau pihak lain, dan
                 tidak menerima, tidak menawarkan atau tidak menjanjikan untuk memberi atau menerima hadiah, imbalan
                 berupa apa saja kepada siapapun yang diketahui atau patut diduga berkaitan dengan pelaksanaan program
-                Corporate Social Responsibility ini (penyuapan).
+                <i>Corporate Social Responsibility</i> ini (penyuapan).
             </li>
 
             <li>
-                <strong>PARA PIHAK</strong> menyepakati bahwa dalam pelaksanaan program Corporate Social Responsibility
+                <strong>PARA PIHAK</strong> menyepakati bahwa dalam pelaksanaan program <i>Corporate Social Responsibility</i>
                 ini selalu mengambil tindakan yang cukup untuk memastikan <strong>PARA PIHAK</strong> patuh terhadap
                 setiap hukum Indonesia yang berlaku, tidak terbatas pada Undang-Undang Nomor 31 Tahun 1999 Juncto
                 Undang-Undang Nomor 20 Tahun 2001 tentang Pemberantasan Tindak Pidana Korupsi serta bersedia dikenakan
@@ -396,8 +400,8 @@
             </li>
 
             <li>
-                <strong>PIHAK KESATU</strong> dengan ini menjamin dalam pelaksanaan program Corporate Social
-                Responsibility ini tidak menyalahgunakan uang dan/atau dana bantuan selain untuk tujuan sebagaimana
+                <strong>PIHAK KESATU</strong> dengan ini menjamin dalam pelaksanaan program <i>Corporate Social
+                Responsibility</i> ini tidak menyalahgunakan uang dan/atau dana bantuan selain untuk tujuan sebagaimana
                 diatur dalam Kesepakatan Kerjasama ini, tidak di bawah pengaruh kepentingan <strong>PIHAK KEDUA</strong>
                 atau <strong>pihak lainnya</strong> dalam mengambil tindakan atau keputusan, serta tidak menerima
                 kontribusi, pemberian uang, komisi politik, atau hal lainnya yang bernilai dari <strong>PIHAK KEDUA</strong>
@@ -405,10 +409,10 @@
             </li>
 
             <li>
-                <strong>PIHAK KEDUA</strong> selaku penerima bantuan program Corporate Social Responsibility menjamin
+                <strong>PIHAK KEDUA</strong> selaku penerima bantuan program <i>Corporate Social Responsibility</i> menjamin
                 tidak akan menawarkan, menjanjikan, memberikan kontribusi, melakukan penyuapan, dan/atau memberikan
                 manfaat lain kepada pegawai <strong>PIHAK KESATU</strong>, serta tidak menyalahgunakan dana bantuan
-                program Corporate Social Responsibility tersebut selain untuk tujuan dalam Kesepakatan Kerja Sama ini.
+                program <i>Corporate Social Responsibility</i> tersebut selain untuk tujuan dalam Kesepakatan Kerja Sama ini.
             </li>
 
             <li>
